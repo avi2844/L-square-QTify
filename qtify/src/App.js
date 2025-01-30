fae2bc511aab6c67e6ed1d56aef8d08a4e04c9d7
Avinash Thakur <avinash.thakur2209@gmail.com>
2025-01-30 23:45:23 +0530

    Fixed the browser router component

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Navbar />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+    </BrowserRouter>
   );
 }
 
