486d88800d7b316559a6b0985d32911ec76e1a7b
Avinash Thakur <avinash.thakur2209@gmail.com>
2025-01-31 00:33:37 +0530

    Added Hero Component

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Hero />
       </div>
     </BrowserRouter>
   );
